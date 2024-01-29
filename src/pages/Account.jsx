c7d8f5fd2eeebcad392afdeb3b2d7f5bfde92780
Josue Tapia <josue.tapia@live.com.ar>
2024-01-28 23:52:13 -0300

    :sparkles: [ADD] edit user profile data

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,17 +1,19 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { ProductContext } from '../context'
 import { getUserByUsername } from '../utils'
+import { EditProfile } from '../components/EditProfile'
 
 export function Account() {
+	const [editUserData, setEditUserData] = useState(false)
 	const { username, usersExisting, setUsername } = useContext(ProductContext)
-
 	const { email } = getUserByUsername({ username, usersExisting })
 
 	const firstTwoLetters = username.substring(0, 2).toUpperCase()
 
 	return (
 		<div className="mx-auto my-10 grid max-w-lg justify-center">
+			<div></div>
 			<div className="flex justify-center">
 				<span className="grid h-32 w-32 place-content-center rounded-full bg-blue-500 text-7xl font-bold text-white">
 					{firstTwoLetters}
@@ -24,7 +26,11 @@ export function Account() {
 			</div>
 
 			<div className="mt-10 text-center">
-				<button className="rounded-full bg-blue-500 px-4 py-2 text-white hover:bg-blue-600">
+				<button
+					type="button"
+					onClick={() => setEditUserData(true)}
+					className="rounded-full bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
+				>
 					Edit Profile
 				</button>
 			</div>
@@ -36,6 +42,7 @@ export function Account() {
 			>
 				Log Out
 			</Link>
+			{editUserData && <EditProfile setEditUserData={setEditUserData} />}
 		</div>
 	)
 }
