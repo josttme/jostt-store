621b502d93de5347a11876778c7180494d283b9f
Josue Tapia <josue.tapia@live.com.ar>
2024-03-13 21:12:15 -0300

    :sparkles: [CHANGE] view likes with Redux State

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,13 +1,13 @@
 import { useContext } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 import { useNavigation } from '@utils'
 import { ProductContext } from '@context'
 import { CardProduct } from '@components/Card'
 
 export function Favorites() {
-	const { addToCart, favorites, toggledFavorites, isFavorite } =
-		useContext(ProductContext)
-
+	const { addToCart } = useContext(ProductContext)
+	const favorites = useSelector((state) => state.storeLikes.likes)
 	const navigate = useNavigate()
 
 	const { navigateToProduct } = useNavigation(navigate)
@@ -17,8 +17,6 @@ export function Favorites() {
 			<CardProduct
 				products={favorites}
 				addToCart={addToCart}
-				isFavorite={isFavorite}
-				toggledFavorites={toggledFavorites}
 				navigateToProduct={navigateToProduct}
 				favorites
 			/>
