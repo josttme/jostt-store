a35340b4c5e64ca42a23f78e32d3a7cca7f2ec6f
Josue Tapia <josue.tapia@live.com.ar>
2024-03-20 23:03:02 -0300

    :sparkles: [UPDATE] Cart Selectors

--- src/redux/slices/cart/cartSelectors.js
+++ src/redux/slices/cart/cartSelectors.js
@@ -6,13 +6,3 @@ export const selectTotalQuantity = createSelector(
 	selectCartItems,
 	(cartItems) => cartItems.reduce((total, item) => total + item.quantity, 0)
 )
-
-export const selectTotalPrice = createSelector(selectCartItems, (cartItems) =>
-	cartItems.reduce((total, item) => total + item.price * item.quantity, 0)
-)
-
-export const selectProductSubtotal = createSelector(
-	(state, productId) =>
-		selectCartItems(state).find((item) => item.id === productId),
-	(product) => (product ? product.price * product.quantity : 0)
-)
