c0c9582311d69dbf46c7fd86b2d65a2d9981eeb6
Josue Tapia <josue.tapia@live.com.ar>
2024-03-24 23:15:52 -0300

    :sparkles: [UPDATE] remove user modal component

--- src/components/account/RemoveUserModal.jsx
+++ src/components/account/RemoveUserModal.jsx
@@ -1,10 +1,17 @@
 import PropTypes from 'prop-types'
-import { useAuthRemoveUser } from '../../hooks/auth/useAuthRemoveUser'
 import { CustomButton } from './CustomButton'
+import { useDispatch } from 'react-redux'
+import { clearCurrentUser, removeUser } from '../../redux/slices'
 
 export function RemoveUserModal({ username, setCloseModal, closeModal }) {
 	if (!closeModal) return
-	const { removeUser } = useAuthRemoveUser({ username })
+
+	const dispatch = useDispatch()
+
+	const handleRemoveUser = () => {
+		dispatch(removeUser({ username }))
+		dispatch(clearCurrentUser())
+	}
 	return (
 		<section className="fixed inset-0  z-50 mx-auto  grid h-screen w-full place-content-center bg-black/20">
 			<div className=" mx-5 grid h-40 grid-cols-2 grid-rows-2 items-end justify-items-center gap-5 rounded-lg  bg-white p-5 sm:w-full sm:max-w-lg ">
@@ -19,7 +26,7 @@ export function RemoveUserModal({ username, setCloseModal, closeModal }) {
 					Cancel
 				</CustomButton>
 				<CustomButton
-					onClick={removeUser}
+					onClick={handleRemoveUser}
 					className="bg-[#dc2626] hover:bg-[#d50000]"
 				>
 					Remove
