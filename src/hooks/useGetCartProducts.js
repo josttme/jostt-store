33280d91793893624352e22e4e3961615f02a935
Josue Tapia <josue.tapia@live.com.ar>
2024-03-25 23:41:23 -0300

    :sparkles: [ADD] new hook get products

---
+++ src/hooks/useGetCartProducts.js
@@ -0,0 +1,23 @@
+import { useSelector } from 'react-redux'
+import { getCurrentUserDetails } from '../redux/slices/users/usersSelectors'
+import { selectCartItems } from '../redux/slices'
+
+export const useGetCartProducts = () => {
+	const username = useSelector(getCurrentUserDetails)
+	const cartProducts = useSelector(selectCartItems)
+
+	// Definimos una función para calcular la cantidad total de productos
+	const calculateTotalQuantity = (products) => {
+		return products.reduce((total, product) => {
+			return total + product.quantity
+		}, 0)
+	}
+
+	if (username) {
+		const quantityProducts = calculateTotalQuantity(username.cartItems)
+		return { cartProducts: username.cartItems, quantityProducts }
+	}
+
+	const quantityProducts = calculateTotalQuantity(cartProducts)
+	return { cartProducts, quantityProducts }
+}
