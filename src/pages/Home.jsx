6bf7b818414085177ade391dd54b1cfbfab30b07
Josue Tapia <josue.tapia@live.com.ar>
2024-03-26 01:49:36 -0300

    :hammer: [REFACTOR] Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,50 +1,31 @@
-import { useContext } from 'react'
-import { useLocation, useNavigate } from 'react-router-dom'
-import { CardProduct } from '@components/Card'
-import { ProductContext } from '@context'
-import { Search } from '@components/Search'
-import { Pagination } from '@components/Pagination'
-import { useNavigation, useResetScroll } from '@utils'
-import { useProductsSearch } from '@hooks/useProductsSearch'
 import { usePagination } from '@hooks/usePagination'
 import { useGetProducts } from '@hooks/useGetProducts'
+import { ProductsList } from '@components/Card'
+import { Search } from '@components/Search'
+import { Pagination } from '@components/Pagination'
+import { useResetScroll } from '../utils'
 
 export function Home() {
-	const { addToCart, isFavorite, toggledFavorites } = useContext(ProductContext)
-	const { allProducts } = useGetProducts()
+	const { products, isLoading, onSearch } = useGetProducts()
 
-	const { productsSearch, getProducts } = useProductsSearch()
+	const { totalPages, firstIndex, lastIndex, onPageChange, currentPage } =
+		usePagination({ products })
 
-	const { products, postsPerPage, setCurrentPage, currentPage } = usePagination(
-		{ allProducts, productsSearch }
-	)
-
-	const navigate = useNavigate()
-	const { navigateToProduct } = useNavigation(navigate)
-
-	// Scroll al principio de la página cuando se cambia de ruta.
-	const { pathname } = useLocation()
-	useResetScroll([pathname])
+	useResetScroll([currentPage])
 	return (
 		<section className=" grid min-h-[calc(100vh-3.5rem)] w-full grid-cols-1 grid-rows-[auto_1fr_auto] pb-10 ">
-			<Search
-				getProducts={getProducts}
-				productsSearch={productsSearch}
-				allProducts={allProducts}
-			/>
+			<Search products={products} isLoading={isLoading} onSearch={onSearch} />
 
-			<CardProduct
+			<ProductsList
 				products={products}
-				addToCart={addToCart}
-				isFavorite={isFavorite}
-				toggledFavorites={toggledFavorites}
-				navigateToProduct={navigateToProduct}
+				isLoading={isLoading}
+				firstIndex={firstIndex}
+				lastIndex={lastIndex}
 			/>
 
 			<Pagination
-				totalPosts={allProducts?.length}
-				postsPerPage={postsPerPage}
-				setCurrentPage={setCurrentPage}
+				totalPages={totalPages}
+				onPageChange={onPageChange}
 				currentPage={currentPage}
 			/>
 		</section>
