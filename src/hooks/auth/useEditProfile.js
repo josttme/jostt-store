12aece894acd5fd1e873df7e0ad07bf628955575
Josue Tapia <josue.tapia@live.com.ar>
2024-03-26 02:30:56 -0300

    :sparkles: [ADD] hook handle edit profile

---
+++ src/hooks/auth/useEditProfile.js
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import { editUser, setCurrentUser } from '../../redux/slices'
+import { isExist } from './authUtils'
+
+export function useAuthEditProfile(users, currentUser, dispatch) {
+	const [messageError, setMessageError] = useState('')
+
+	const handleEditProfile = (e) => {
+		e.preventDefault()
+
+		const newUserData = Object.fromEntries(new FormData(e.target))
+		const { username, email, password } = newUserData
+
+		const isUserExist = isExist(users, 'username', username)
+		const isEmailExist = isExist(users, 'email', email)
+
+		const updateUserData = (users, currentUser) => {
+			const user = users.find((u) => u.username === currentUser)
+			return { ...user, username, email, password }
+		}
+		const editUserData = () => {
+			const updateUser = updateUserData(users, currentUser)
+			setMessageError('')
+			dispatch(setCurrentUser({ username }))
+			dispatch(editUser({ updateUser, currentUser }))
+			e.target.reset()
+		}
+		isUserExist
+			? setMessageError('The user already exists')
+			: isEmailExist
+				? setMessageError('The email is already in use')
+				: editUserData()
+	}
+	return { handleEditProfile, messageError }
+}
