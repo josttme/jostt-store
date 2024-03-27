428b358f758709638716492a633eddfcc99f337c
Josue Tapia <josue.tapia@live.com.ar>
2024-03-27 01:50:01 -0300

    :hammer: [REFACTOR] product page

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,26 +1,26 @@
-import { useContext } from 'react'
-import { useLocation, useParams } from 'react-router-dom'
-import { ProductContext } from '@context'
-import { SvgHeart } from '@components/icons/SvgHeart'
+import { useParams } from 'react-router-dom'
+import { useGetProductsQuery } from '../redux/api'
 import { ProductInfo } from '@components/ProductInfo'
 import { ProductImage } from '@components/ProductImage'
 import { CategoryButton } from '@components/CategoryButton'
+import { AddToCartButton } from '@components/AddToCartButton'
+import { FavoriteButton } from '@components/FavoriteButton'
 import { getProductById } from '@services/products'
-import { useGetProducts } from '@hooks/useGetProducts'
 import { useResetScroll } from '@utils'
 
 export function Product() {
-	const { isFavorite, toggledFavorites, addToCart } = useContext(ProductContext)
-
 	const { id } = useParams()
-
-	const { allProducts } = useGetProducts()
-
-	const product = getProductById({ id, allProducts })
+	const idProduct = Number(id)
 
 	// Scroll al principio de la página cuando se cambia de ruta.
-	const { pathname } = useLocation()
-	useResetScroll([pathname])
+	useResetScroll([idProduct])
+
+	const { data } = useGetProductsQuery()
+	if (!data) return
+	const allProducts = data?.products
+
+	const product = getProductById({ idProduct, allProducts })
+
 	if (!product) return
 	const {
 		name,
@@ -33,8 +33,6 @@ export function Product() {
 		createdByLink,
 		sourceLink
 	} = product
-
-	const isFavorited = isFavorite(product)
 
 	return (
 		<section className=" grid gap-4 overflow-hidden bg-white   lg:mx-auto  lg:my-28 lg:max-w-6xl  lg:grid-cols-[repeat(auto-fit,minmax(550px,550px))]  lg:rounded-lg lg:shadow-lg">
@@ -58,27 +56,9 @@ export function Product() {
 					productPage
 				/>
 				<p className="col-start-1 text-4xl font-bold text-red-6">{`$${price}`}</p>
-				<div className="col-span-2  flex w-full justify-between gap-4 lg:justify-start">
-					<button
-						type="button"
-						onClick={(e) => addToCart(e, product)}
-						className="relative grid w-4/5  items-center rounded-lg bg-gradient-to-r from-red-6  to-red-9 p-3 text-center text-lg    text-white  before:absolute before:h-full before:w-full before:rounded-lg hover:before:bg-black/10 lg:w-60"
-					>
-						Add to cart
-					</button>
-
-					<button
-						type="button"
-						onClick={(e) => toggledFavorites(e, product)}
-						className="grid aspect-square h-full w-auto place-content-center rounded-full bg-slate-200  stroke-black p-2 shadow-sm transition duration-300 hover:stroke-red-6  "
-					>
-						<SvgHeart
-							className={`
-								${isFavorited ? 'fill-red-5 stroke-red-5' : 'fill-none'} h-6 w-6
-							`}
-							strokeWidth="1.3"
-						/>
-					</button>
+				<div className="relative col-span-2  flex w-full justify-between gap-4 lg:justify-start">
+					<AddToCartButton product={product} />
+					<FavoriteButton product={product} className="h-8 w-8" />
 				</div>
 			</div>
 		</section>
