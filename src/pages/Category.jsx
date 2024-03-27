07dc2326da44c98fb198e770e211524af10d7e79
Josue Tapia <josue.tapia@live.com.ar>
2024-03-27 01:54:47 -0300

    :hammer: [REFACTOR] category page

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,36 +1,23 @@
-import { useContext } from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
-import { useNavigation, useResetScroll } from '@utils'
-import { CardProduct } from '@components/Card'
-import { ProductContext } from '@context'
+import { useParams } from 'react-router-dom'
+import { useGetProductsQuery } from '../redux/api'
+import { ProductsList } from '@components/Card'
 import { getProductsByCategory } from '@services/products'
-import { useGetProducts } from '@hooks/useGetProducts'
+import { useResetScroll } from '@utils'
 
 export function Category() {
-	const { addToCart, isFavorite, toggledFavorites } = useContext(ProductContext)
-	const { allProducts } = useGetProducts()
-	const navigate = useNavigate()
-
 	// Obtiene el parámetro "path" de la ruta actual
 	const { id } = useParams()
 	// Divide el "path" en partes
 	const idParts = id.split('-')
 	// Obtiene el último elemento de la ruta actual
-	const categoryId = idParts[idParts.length - 1]
-
+	const categoryId = Number(idParts[idParts.length - 1])
+	useResetScroll([id])
+	const { data } = useGetProductsQuery()
+	if (!data) return
+	const allProducts = data?.products
 	const products = getProductsByCategory({ categoryId, allProducts })
 
-	const { navigateToProduct } = useNavigation(navigate)
-
 	// Scroll al principio de la página cuando se cambia de ruta.
-	useResetScroll([id])
-	return (
-		<CardProduct
-			products={products}
-			addToCart={addToCart}
-			isFavorite={isFavorite}
-			toggledFavorites={toggledFavorites}
-			navigateToProduct={navigateToProduct}
-		/>
-	)
+
+	return <ProductsList products={products} />
 }
