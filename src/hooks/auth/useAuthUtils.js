b4b768877a1eed9cda24b479348a2ffa334095f2
Josue Tapia <josue.tapia@live.com.ar>
2024-03-27 02:26:16 -0300

    :fire: [REMOVE] hooks files replaced

--- src/hooks/auth/useAuthUtils.js
+++
@@ -1,5 +0,0 @@
-export function useAuthUtils() {
-	const isExist = (property, newUser, usersExisting) =>
-		usersExisting.some((user) => user[property] === newUser[property])
-	return { isExist }
-}
