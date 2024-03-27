31cdf9849e7ef66a62f8968c1d95b74001cd2483
Josue Tapia <josue.tapia@live.com.ar>
2024-03-27 02:07:49 -0300

    :poop: [REFACTOR] context

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -1,80 +1,12 @@
 import PropTypes from 'prop-types'
-import { createContext, useEffect, useState } from 'react'
-import { useToggleFavotites } from '@hooks/useToggleFavotites'
-import { useCart } from '@hooks/useCart'
-import { useSessionStorage } from '@hooks/useSessionStorage'
-import useLocalStorage from '@hooks/useLocalStorage'
+import { createContext, useState } from 'react'
 
 export const ProductContext = createContext()
 
 export function ProductProvider({ children }) {
-	const [allProducts, setAllProducts] = useState([])
-	const [productsLoaded, setProductsLoaded] = useState(false)
-	const [quantityProducts, setQuantityProducts] = useState(0)
-
-	const [usersExisting, updateUsers] = useLocalStorage('users_store')
-
-	const [username, setUsername] = useSessionStorage('username')
-	const [errorCheckout, setErrorCheckout] = useState('')
-	const [favorites, toggledFavorites, setProducts] = useToggleFavotites(
-		'store_favorites',
-		usersExisting,
-		username,
-		updateUsers
-	)
-	const [
-		cartItems,
-		addToCart,
-		removeFromCart,
-		increaseQuantity,
-		decreaseQuantity,
-		setCartItems
-	] = useCart(
-		'store_cart',
-		usersExisting,
-		username,
-		updateUsers,
-		favorites,
-		setProducts
-	)
-
 	const [openNavBar, setOpenNavBar] = useState(false)
 
-	const isFavorite = (product) => {
-		return favorites.some((item) => item.id === product.id)
-	}
-
-	useEffect(() => {
-		// Suma el total de la cantidad de productos en el carrito
-		setQuantityProducts(
-			cartItems.reduce((setQuantityProducts, item) => {
-				return setQuantityProducts + item.quantity
-			}, 0)
-		)
-	}, [cartItems])
-
 	const valueContext = {
-		favorites,
-		toggledFavorites,
-		isFavorite,
-		cartItems,
-		addToCart,
-		removeFromCart,
-		increaseQuantity,
-		decreaseQuantity,
-		setCartItems,
-		quantityProducts,
-		allProducts,
-		productsLoaded,
-		setAllProducts,
-		setProductsLoaded,
-		usersExisting,
-		updateUsers,
-		username,
-		setUsername,
-		errorCheckout,
-		setErrorCheckout,
-		setProducts,
 		openNavBar,
 		setOpenNavBar
 	}
