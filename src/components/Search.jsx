cd843c6e76e9084feff0a008810133bdba1a4c75
Josue Tapia <josue.tapia@live.com.ar>
2024-03-27 02:08:50 -0300

    :hammer: [REFACTOR] search component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,51 +1,37 @@
-import { useCallback } from 'react'
 import { PropTypes } from 'prop-types'
-import debounce from 'just-debounce-it'
-import { SvgSearch } from '@components/icons/SvgSearch'
 import { useSearch } from '@hooks/useSearch'
+import { SvgSearch } from '@components/icons/SvgSearch'
 
-export function Search({ getProducts, productsSearch, allProducts }) {
-	const { search, updateSearch, errorMessage } = useSearch({ productsSearch })
-	const debounceGetProducts = useCallback(
-		debounce((search) => {
-			getProducts({ search, allProducts })
-		}, 300),
-		[getProducts, allProducts]
-	)
-
-	const handleChange = (e) => {
-		const newQuery = e.target.value
-
-		if (newQuery.startsWith(' ')) return
-		debounceGetProducts(newQuery)
-		updateSearch(newQuery)
-	}
+export function Search({ products, onSearch, isLoading }) {
+	const { handleChange, errorMessage } = useSearch({
+		products,
+		isLoading,
+		onSearch
+	})
 
 	return (
 		<div className="mx-auto pt-5 iphone678:w-4/5 iphone678:max-w-md">
-			<form
-				onSubmit={(e) => e.preventDefault()}
-				className="mb-4 flex h-11 w-full  items-stretch"
-			>
+			<form className="mb-4 flex h-11 w-full  items-stretch">
 				<input
 					className=" flex-auto rounded-bl-lg rounded-tl-lg  border  border-gray-300 bg-white  px-3 py-1.5  text-gray-700  focus:border-gray-500 focus:outline-none"
 					placeholder="Search"
 					onChange={handleChange}
 					type="search"
-					value={search}
 				/>
 				<span className="flex items-center rounded-br-lg rounded-tr-lg bg-gray-900 px-6 py-2.5">
 					<SvgSearch className="h-4 w-4 fill-white" />
 				</span>
 			</form>
 			<div className={`h-6 w-full transition-all `}>
-				<p className="text-center  text-red-6">{errorMessage}</p>
+				{errorMessage && (
+					<p className="text-center  text-red-6">{errorMessage}</p>
+				)}
 			</div>
 		</div>
 	)
 }
 Search.propTypes = {
-	getProducts: PropTypes.func.isRequired,
-	productsSearch: PropTypes.array,
-	allProducts: PropTypes.array
+	products: PropTypes.array,
+	isLoading: PropTypes.bool,
+	onSearch: PropTypes.func
 }
